f6442785b4c901ed4e7d346f36bcabdff904f402
ellie25v <58464480+ellie25v@users.noreply.github.com>
2020-01-16 20:59:42 +0200

    A (#27)
    * adjusting here and there
    
    * vefer

---
+++ src/js/script.js
@@ -0,0 +1,11 @@
+const menu_open_btn = document.querySelector('.menu');
+const menu_close_btn = document.querySelector('.menu-dropdown::after');
+const menu_nav = document.querySelectorAll('.menu-dropdown');
+
+// menu_open_btn.addEventListener('click', () => {
+// 	menu_nav.forEach(nav_el => { nav_el.classList.add('visible') });
+// });
+
+// menu_close_btn.addEventListener('click', () => {
+// 	menu_nav.forEach(nav_el => { nav_el.classList.remove('visible') });
+// });
