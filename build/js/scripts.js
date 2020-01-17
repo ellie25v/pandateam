f4e70689419e9300f1f573256f0b3c51cb4125af
ellie25v <58464480+ellie25v@users.noreply.github.com>
2020-01-17 10:52:26 +0200

    Dev (#30)
    * test dev
    
    * init section direction
    
    * try
    
    * add html and a bit css
    
    * add tablet version
    
    * adjust menu and change hover on it
    
    * adjust hover on button and menu
    
    * add html and pics
    
    * mobile version
    
    * add tablet ver
    
    * add desktop ver
    
    * section direction was added with styles
    
    * add direction
    
    * box-sizing
    
    * adding
    
    * dsfhdjf
    
    * button hover works ok
    
    * add of section-program
    
    * sdjhfsdf
    
    * id added
    
    * dd
    
    * added gitignore
    
    * added section structure
    
    * add gitignore
    
    * deleted node modules
    
    * deleting node
    
    * del node mod
    
    * corection for direction after marge
    
    * corection for program after marge
    
    * without nodemodules
    
    * changed menu hover so text wrapper disappear
    
    * adjust hover on menu so text dxt disappear
    
    * adjust hover on menu
    
    * added footer without node modules
    
    * gjl
    
    * cildren
    
    * fbjhetvkk
    
    * gehj
    
    * bhf
    
    * fbk
    
    * A (#27)
    
    * adjusting here and there
    
    * vefer
    
    * A (#28)
    
    * adjusting here and there
    
    * vefer
    
    * header problems still there
    
    * A (#29)
    
    * adjusting here and there
    
    * vefer
    
    * header problems still there
    
    * hbfv
    
    Co-authored-by: kravolya <57864830+kravolya@users.noreply.github.com>
    Co-authored-by: grey4uk <57910084+grey4uk@users.noreply.github.com>
    Co-authored-by: VladyslavGit <58343740+VladyslavGit@users.noreply.github.com>

---
+++ build/js/scripts.js
@@ -0,0 +1,18 @@
+
+"use strict";
+
+var burgerBtn = document.querySelector('.menu');
+var burgerBtnClose = document.querySelector('.menu-dropdown-btn');
+var burgerMenu = document.querySelector('.menu-dropdown');
+
+var openMenu = function openMenu() {
+  burgerMenu.classList.add('visible');
+};
+
+var closeMenu = function closeMenu() {
+  burgerMenu.classList.remove('visible');
+};
+
+burgerBtn.addEventListener('click', openMenu);
+burgerBtnClose.addEventListener('click', closeMenu);
+
