5cb104fe36edc47511f198555575c527184f1e34
ellie25v <58464480+ellie25v@users.noreply.github.com>
2020-01-17 10:49:52 +0200

    A (#29)
    * adjusting here and there
    
    * vefer
    
    * header problems still there
    
    * hbfv

--- build/js/scripts.js
+++ build/js/scripts.js
@@ -1,8 +1,18 @@
-const menu_open_btn = document.querySelector('.menu');
-const menu_close_btn = document.querySelector('.menu-dropdown::after');
-const menu_nav = document.querySelectorAll('.menu-dropdown'); // menu_open_btn.addEventListener('click', () => {
-// 	menu_nav.forEach(nav_el => { nav_el.classList.add('visible') });
-// });
-// menu_close_btn.addEventListener('click', () => {
-// 	menu_nav.forEach(nav_el => { nav_el.classList.remove('visible') });
-// });
+
+"use strict";
+
+var burgerBtn = document.querySelector('.menu');
+var burgerBtnClose = document.querySelector('.menu-dropdown-btn');
+var burgerMenu = document.querySelector('.menu-dropdown');
+
+var openMenu = function openMenu() {
+  burgerMenu.classList.add('visible');
+};
+
+var closeMenu = function closeMenu() {
+  burgerMenu.classList.remove('visible');
+};
+
+burgerBtn.addEventListener('click', openMenu);
+burgerBtnClose.addEventListener('click', closeMenu);
+
